bbf35d76617d4e20ec4b04f4e621683bca2fa9ab
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-22 23:36:02 +0100

    add tailwind plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,12 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwindcss-elevation")(["responsive"]),
+    require("tailwindcss-no-scrollbar"),
+  ],
 };
