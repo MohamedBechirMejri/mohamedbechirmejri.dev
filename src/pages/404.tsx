2e9abc2cafca0b6443c52e5d2646b2a4d2f37c2f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 19:36:59 +0100

    update 404 head

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const Custom404 = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-green-100 text-center text-5xl font-bold text-green-900 transition-all lg:text-8xl">
-      <h1>
-        404 <br /> Under Construction
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Not Found</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex h-full w-full items-center justify-center bg-green-100 text-center text-5xl font-bold text-green-900 transition-all lg:text-8xl">
+        <h1>
+          404 <br /> Under Construction
+        </h1>
+      </div>
+    </>
   );
 };
 
