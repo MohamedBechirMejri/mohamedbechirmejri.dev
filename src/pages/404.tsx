f1271e17f1cca7fe3fcbd9db92c1ddbb58702b25
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 17:49:55 +0100

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+const Custom404 = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center bg-green-100 text-center text-8xl font-bold text-green-900">
+      <h1>
+        404 <br /> Under Construction
+      </h1>
+    </div>
+  );
+};
+
+export default Custom404;
