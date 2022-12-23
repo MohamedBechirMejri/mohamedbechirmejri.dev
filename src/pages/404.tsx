f5d41ac624addf8b0e018e68a72410534faf77a1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 17:52:17 +0100

    make 404 responsive

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 const Custom404 = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-green-100 text-center text-8xl font-bold text-green-900">
+    <div className="flex h-full w-full items-center justify-center bg-green-100 text-center text-5xl font-bold text-green-900 transition-all lg:text-8xl">
       <h1>
         404 <br /> Under Construction
       </h1>
