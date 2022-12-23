6e5d5d90efdf4299cb8c7146ec86a309f98f4fc4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 02:28:34 +0100

    put projects data  in json

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,54 +1,6 @@
-import { useState } from "react";
+import projects from "../../public/json/projects.json";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([
-    {
-      name: "nyutab",
-      description: "A Better NewTab page for Chrome/FireFox (WIP)",
-      tech: ["ReactJS", "TailwindCSS"],
-      demo: "https://google.com",
-      source: "https://github.com/MohamedBechirMejri/nyutab",
-    },
-    {
-      name: "Facebook Clone",
-      description: "TheOdinProject's Capstone project",
-      tech: [
-        "Next.js",
-        "Tailwind CSS",
-        "Firebase",
-        "MongoDB",
-        "TypeScript",
-        "Websocket",
-        "More..",
-      ],
-      demo: "https://odinbook-next.vercel.app/",
-      source: "https://github.com/MohamedBechirMejri/facebook-next",
-    },
-    {
-      name: "Etch-A-Sketch",
-      description:
-        "Replicating the original Etch-A-Sketch Toy. I'm proud of this one.",
-      tech: ["HTML", "CSS", "JavaScript"],
-      demo: "https://mohamedbechirmejri.github.io/ETCH-A-SKETCH/",
-      source: "https://github.com/MohamedBechirMejri/ETCH-A-SKETCH",
-    },
-    {
-      name: "Unlimited Wordle",
-      description:
-        "My first TS project, Wordle Clone with unlimited rounds per day. made in the Style of Motus because dad loves that show. ",
-      tech: ["TypeScript", "ReactJS", "Tailwind CSS"],
-      demo: "https://mohamedbechirmejri.github.io/unlimited-wordle",
-      source: "https://github.com/MohamedBechirMejri/unlimited-wordle",
-    },
-    {
-      name: "Photo Tagging Game",
-      description: "Character finding game with global realtime leaderboard. ",
-      tech: ["ReactJS", "Firebase", "Tailwind CSS"],
-      demo: "https://photo-tagging-game-6d738.web.app/",
-      source:
-        "https://github.com/MohamedBechirMejri/where-s-waldo-a-photo-tagging-app",
-    },
-  ]);
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center gap-24">
       <h1 className="text-4xl font-bold">My Projects</h1>
@@ -56,7 +8,7 @@ const Projects = () => {
         {projects.map((p, i) => (
           <div
             key={"project#" + i}
-            className="flex h-[24rem] w-[20rem] flex-col items-center justify-evenly overflow-hidden rounded-3xl border border-current p-4 ring-current transition-all duration-500 hover:rounded-[2.5rem] hover:font-medium hover:ring "
+            className="flex h-[24rem] w-[20rem] flex-col items-center justify-evenly overflow-hidden rounded-3xl border border-current p-4 ring-current transition-all duration-300 hover:rounded-[2.5rem] hover:font-medium hover:ring"
           >
             <h2 className="text-3xl font-semibold">{p.name}</h2>
             <p>
