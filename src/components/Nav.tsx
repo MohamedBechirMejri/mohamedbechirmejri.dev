f4e6650ccf4ad1fa6b1603a17cf6357c74543607
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 19:18:34 +0100

    add accessibility features

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -58,7 +58,17 @@ const Nav = ({ children }: { children: React.ReactNode }) => {
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         className="absolute bottom-10 left-1/2 z-20 h-[3.5rem] w-[3.5rem] rounded-full elevation-8"
-        onClick={() => setIsOpen(!isOpen)}
+        aria-label="toggle nav"
+        aria-pressed="false"
+        onClick={(e) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          const target: HTMLButtonElement = e.target;
+          const pressed = target.getAttribute("aria-pressed") === "true";
+          target.setAttribute("aria-pressed", `${!pressed}`);
+
+          setIsOpen(!isOpen);
+        }}
       ></motion.button>
     </div>
   );
