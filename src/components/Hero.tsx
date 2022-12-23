bc4685ca02e835fefb128dc3dc81a40fddb3ca64
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 15:33:54 +0100

    Add Hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center p-8">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-7xl font-bold transition-all sm:text-8xl">
+          Mohamed Bechir <br /> Mejri
+        </h1>
+        <p className="text-3xl">full-stack web developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 
@@ -11,14 +12,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-green-100 p-8 font-[Montserrat] text-green-900 selection:bg-green-900 selection:text-green-100">
-        <div className="flex h-screen w-full items-center justify-center p-8">
-          <div className="flex flex-col gap-4">
-            <h1 className="text-7xl font-bold transition-all sm:text-8xl">
-              Mohamed Bechir <br /> Mejri
-            </h1>
-            <p className="text-3xl">full-stack web developer</p>
-          </div>
-        </div>
+        <Hero />
         <Projects />
         <Contact />
       </div>
