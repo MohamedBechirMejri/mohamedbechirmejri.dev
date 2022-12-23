1f704981792dfb47628be6d7a52d8b3185047c21
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-12-23 15:25:12 +0100

    add profession

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,12 @@ const Home = () => {
       </Head>
       <div className="bg-green-100 p-8 font-[Montserrat] text-green-900 selection:bg-green-900 selection:text-green-100">
         <div className="flex h-screen w-full items-center justify-center p-8">
-          <h1 className="text-8xl font-bold">
-            Mohamed Bechir <br /> Mejri
-          </h1>
+          <div className="flex flex-col gap-4">
+            <h1 className="text-7xl font-bold transition-all sm:text-8xl">
+              Mohamed Bechir <br /> Mejri
+            </h1>
+            <p className="text-3xl">full-stack web developer</p>
+          </div>
         </div>
         <Projects />
         <Contact />
