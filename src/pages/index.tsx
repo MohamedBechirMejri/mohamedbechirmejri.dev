af52416858f67b3058906104974642789689a350
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-02-24 21:42:59 +0100

    scale model

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import Head from "next/head";
 import { Laptop } from "../components/Laptop";
+import { Suspense } from "react";
 
-const Home = () => {
+const Home = (props) => {
   return (
     <>
       <Head>
@@ -14,9 +15,8 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="h-[100svh] w-[100svw]">
-        <Canvas className="bg-black bg-opacity-0">
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
+        <Canvas>
+          <pointLight position={[10, 10, 10]} intensity={1.5} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <spotLight
             position={[-10, -10, -10]}
@@ -31,9 +31,28 @@ const Home = () => {
             castShadow
           />
 
-          <Laptop />
-
-          <OrbitControls />
+          <Suspense fallback={null}>
+            <group
+              rotation={[0, 0, 0]}
+              position={[0, 2, 0]}
+              scale={[14, 14, 14]}
+            >
+              <Laptop />
+            </group>
+            <Environment preset="city" />
+          </Suspense>
+          <ContactShadows
+            position={[0, -4.5, 0]}
+            scale={20}
+            blur={2}
+            far={4.5}
+          />
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            minPolarAngle={Math.PI / 2.2}
+            maxPolarAngle={Math.PI / 2.2}
+          />
         </Canvas>
       </div>
     </>
