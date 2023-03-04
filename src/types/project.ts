f637625f9c49639cc5ab59216f3d5d4833b8e793
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-04 22:14:54 +0100

    add project type

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export type Project = {
+  name: string;
+  description: string;
+  tech: string[];
+  demo: string;
+  sourceCode: string;
+  logo: string;
+  style: object;
+  imgStyle: object;
+};
