cbc0df4677c133dd359d61bc402d20a712458ebe
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-11 05:35:19 +0100

    tweak text position

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,18 +1,11 @@
 import { Text } from "@react-three/drei";
-import { useControls } from "leva";
 
 const Background = () => {
-  const { r, g, b } = useControls({
-    r: { value: 0.5, min: 0, max: 1 },
-    g: { value: 0.5, min: 0, max: 1 },
-    b: { value: 0.5, min: 0, max: 1 },
-  });
-
   return (
     <group>
       <Text
         font="/assets/fonts/bangers.ttf"
-        position={[-8, 2, -5.8]}
+        position={[-8.5, 0.5, -5.8]}
         rotation={[0.42, 0.48, 0.12]}
         castShadow
         maxWidth={2}
@@ -22,12 +15,15 @@ const Background = () => {
       </Text>
       <Text
         font="/assets/fonts/bangers.ttf"
-        position={[5.6, 0, -2.4]}
+        position={[6, 0, -2.4]}
         rotation={[1.26, -1.38, 1]}
         castShadow
+        maxWidth={2}
+        fontSize={2}
+        textAlign="center"
       >
         Full Stack Dev
-        <meshBasicMaterial color={[r, g, b]} toneMapped={false} />
+        <meshBasicMaterial color={"#1f1818"} toneMapped={false} />
       </Text>
     </group>
   );
