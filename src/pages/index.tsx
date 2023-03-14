2154e0585e0c8a03361f17d7be7d126fdcf4147d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-14 03:42:53 +0100

    remove notice

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="h-[100svh] w-[100svw]">
-        {/* <Scene /> */}
-        <h1 className="fixed flex h-[100svh] w-[100svw] items-center justify-center bg-gradient-to-br from-[#764BA2] to-[#E2D1C3] text-3xl font-bold xl:text-8xl">
-          In Development...
-        </h1>
+        <Scene />
         <Footer />
       </div>
     </>
